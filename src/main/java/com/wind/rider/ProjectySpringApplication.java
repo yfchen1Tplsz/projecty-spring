0036aeb5ce44c581d23c46630f242458e8c87ef9
yfchen1 <1634807103@qq.com>
2024-03-26 01:18:12 +0800

    feat： new project, show my note during learning spring. date: 2024-03-26 1:17 author: summer3

---
+++ src/main/java/com/wind/rider/ProjectySpringApplication.java
@@ -0,0 +1,13 @@
+package com.wind.rider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectySpringApplication.class, args);
+    }
+
+}
